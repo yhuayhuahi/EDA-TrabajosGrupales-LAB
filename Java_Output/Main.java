a59d415dfbfa74f67d7b1a3293fd7eb895264e0e
Jeremy Perez <jperezhua@unsa.edu.pe>
2024-05-05 17:08:28 -0500

    Añadimos los outputs

---
+++ Java_Output/Main.java
@@ -0,0 +1,19 @@
+ class Main {
+
+	    public static void main(String[] args) {
+			System.out.println("Hello world");
+			System.out.println("I am learning Java");
+			System.out.println("It is awesome");
+
+		      //Errores de sintaxis
+
+			System.out.println("This sentence will work!");
+		      //System.out.println(This sentence will produce an error);
+
+		      //Imprimir en la misma linea
+
+		      	System.out.print("Hello World! ");
+			System.out.print("I will print on the same line.");
+		        }
+	        
+}
---
+++ Java_Output/Numbers.java
@@ -0,0 +1,14 @@
+public class numbers {
+
+	    public static void main(String[] args) {
+			System.out.println(3);
+			System.out.println(358);
+			System.out.println(50000);
+
+			//Imprimir operaciones
+
+			System.out.println(3 + 3);
+			System.out.println(2 * 5);
+		        }
+	        
+}
