b927236d0ff6c129851e6ba1082e0112276fb932
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2024-05-05 17:35:45 -0500

    Java_Methods

---
+++ Java_Methods/JavaMethods.java
@@ -0,0 +1,9 @@
+public class JavaMethods {
+    //Insert the missing part to call myMethod from main.
+    public static void main(String[] args) {
+        myMethod();
+    }
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+}
