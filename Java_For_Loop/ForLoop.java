39211e7ff27583ad7fdc4cca20b9ddb7275fab22
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2024-05-05 15:51:45 -0500

    Java For Loop

---
+++ Java_For_Loop/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        //Use a for loop to print "Yes" 5 times.
+        for (int i=0; i<5; i++) {
+            System.out.println("Yes");
+        }
+    }
+}
---
+++ Java_For_Loop/For_EachLoop.java
@@ -0,0 +1,9 @@
+public class For_EachLoop {
+    public static void main(String[] args) {
+        //Loop through the items in the cars array.
+        String[] cars = {"Volvo", "BMW", "Ford"};
+        for (String i: cars) {
+            System.out.println(i);
+        }
+    }
+}
