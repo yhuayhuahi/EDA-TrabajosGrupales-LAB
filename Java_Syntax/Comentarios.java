d487016faf86cded5a087aa3d4bd0f7797703671
Jeremy Perez <jperezhua@unsa.edu.pe>
2024-05-05 16:25:58 -0500

    Segundo ejercicio Sintaxis

---
+++ Java_Syntax/Comentarios.java
@@ -0,0 +1,9 @@
+public class MyClass {
+	  public static void main(String[] args) {
+		// This is a single-line comment
+		//
+    		/*
+     	*  	This is a multi-line comment
+     	*  	*/		  
+	  }
+}
